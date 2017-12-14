9179392efbd4ad892f8af8b9f951bc233f247344
Trung Nguyen <me@ngthanhtrung.com>
2017-12-14 16:35:48 +0700

    Add a method to convert field type to string

--- src/main/scala/anduin/protocompat/report/ProtoReport.scala
+++ src/main/scala/anduin/protocompat/report/ProtoReport.scala
@@ -1,7 +1,34 @@
 package anduin.protocompat.report
 
+import com.google.protobuf.DescriptorProtos.FieldDescriptorProto
+
 import anduin.protocompat.check.ProtoCheckResult
 
 trait ProtoReport {
+
+  protected def fieldType(tpe: FieldDescriptorProto.Type): String = {
+    tpe match {
+      case FieldDescriptorProto.Type.TYPE_DOUBLE => "double"
+      case FieldDescriptorProto.Type.TYPE_FLOAT => "float"
+      case FieldDescriptorProto.Type.TYPE_INT64 => "int64"
+      case FieldDescriptorProto.Type.TYPE_UINT64 => "uint64"
+      case FieldDescriptorProto.Type.TYPE_INT32 => "int32"
+      case FieldDescriptorProto.Type.TYPE_FIXED64 => "fixed64"
+      case FieldDescriptorProto.Type.TYPE_FIXED32 => "fixed32"
+      case FieldDescriptorProto.Type.TYPE_BOOL => "bool"
+      case FieldDescriptorProto.Type.TYPE_STRING => "string"
+      case FieldDescriptorProto.Type.TYPE_GROUP => "group"
+      case FieldDescriptorProto.Type.TYPE_MESSAGE => "message"
+      case FieldDescriptorProto.Type.TYPE_BYTES => "bytes"
+      case FieldDescriptorProto.Type.TYPE_UINT32 => "uint32"
+      case FieldDescriptorProto.Type.TYPE_ENUM => "enum"
+      case FieldDescriptorProto.Type.TYPE_SFIXED32 => "sfixed32"
+      case FieldDescriptorProto.Type.TYPE_SFIXED64 => "sfixed64"
+      case FieldDescriptorProto.Type.TYPE_SINT32 => "sint32"
+      case FieldDescriptorProto.Type.TYPE_SINT64 => "sint64"
+      case _ => "unknown"
+    }
+  }
+
   def report(result: ProtoCheckResult): Unit
 }
