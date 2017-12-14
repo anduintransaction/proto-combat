39cd2756bd1a9ae1d1eed8deb2f3baaa80f48c4e
Trung Nguyen <me@ngthanhtrung.com>
2017-12-14 16:36:18 +0700

    Implement console reporter

--- src/main/scala/anduin/protocompat/report/ConsoleProtoReport.scala
+++ src/main/scala/anduin/protocompat/report/ConsoleProtoReport.scala
@@ -1,10 +1,139 @@
 package anduin.protocompat.report
 
-import anduin.protocompat.check.ProtoCheckResult
+import java.io.File
 
-final class ConsoleProtoReport {
+import anduin.protocompat.check._
+import anduin.protocompat.tree.ProtoTree
+
+final class ConsoleProtoReport(
+  showFullPath: Boolean
+) extends ProtoReport {
+
+  import ConsoleProtoReport._
+
+  private[this] def protoPath(
+    name: String,
+    kind: ProtoKind,
+    basePaths: Map[String, String],
+    tree: ProtoTree
+  ): String = {
+    val optionalProtoAndFile = tree.findProtoAndFile(name)
+
+    require(
+      optionalProtoAndFile.isDefined,
+      s"${kind.capitalizeName} proto with name $name doesn't exist."
+    )
+
+    val file = optionalProtoAndFile.get match { case (_, file) => file }
+    val optionalBasePath = basePaths.get(file.proto.getName)
+
+    require(
+      optionalBasePath.isDefined,
+      s"${kind.capitalizeName} proto file ${file.proto.getName} doesn't exist."
+    )
+
+    s"$name (${optionalBasePath.get}${File.separator}${file.proto.getName})"
+  }
+
+  private[this] def fullPath(
+    path: ProtoCheckPath,
+    kind: ProtoKind,
+    basePaths: Map[String, String],
+    tree: ProtoTree
+  ): String = {
+    val builder = StringBuilder.newBuilder
+
+    builder.append(s"${kind.capitalizeName} proto:$NewLine")
+    builder.append(protoPath(path.name, kind, basePaths, tree))
+    builder.append(NewLine)
+
+    if (showFullPath) {
+      path.supers.foreach {
+        case (name, tagNumber) =>
+          builder.append(s"as tag $tagNumber in ${protoPath(name, kind, basePaths, tree)}$NewLine")
+      }
+    }
+
+    builder.result()
+  }
 
   def report(result: ProtoCheckResult): Unit = {
-    println("OK lah.")
+    val builder = StringBuilder.newBuilder
+
+    result.incompats.foreach { incompat =>
+      incompat.reason match {
+        case FieldRemovedNotReserved(oldTagNumber, oldFieldName) =>
+          builder.append(
+            s"Field $oldFieldName (tag $oldTagNumber) removed but not reserved.$NewLine"
+          )
+
+        case FieldTypeConflicted(
+            tagNumber,
+            newFieldName,
+            newFieldType,
+            oldFieldName,
+            oldFieldType
+            ) =>
+          builder.append(
+            s"Type ${fieldType(oldFieldType)} of old field $oldFieldName (tag $tagNumber)$NewLine"
+          )
+
+          builder.append(
+            s"conflicts with type ${fieldType(newFieldType)} of new field $newFieldName (tag $tagNumber)$NewLine"
+          )
+
+        case ScalaFieldTypeConflicted(
+            tagNumber,
+            oldFieldName,
+            oldScalaFieldType,
+            newFieldName,
+            newScalaFieldType
+            ) =>
+          builder.append(
+            s"Scala type $oldScalaFieldType of old field $oldFieldName (tag $tagNumber)$NewLine"
+          )
+
+          builder.append(
+            s"conflicts with Scala type $newScalaFieldType of new field $newFieldName (tag $tagNumber)$NewLine"
+          )
+
+        case FieldRenamed(
+            tagNumber,
+            oldFieldName,
+            newFieldName
+            ) =>
+          builder.append(s"Old field $oldFieldName (tag $tagNumber)$NewLine")
+          builder.append(s"is renamed to new field $newFieldName (tag $tagNumber)$NewLine")
+
+        case _ =>
+        // Do nothing
+      }
+
+      // In most cases it's more natural to have old path coming first
+      builder.append(fullPath(incompat.oldPath, ProtoKind.Old, result.oldBasePaths, result.oldTree))
+      builder.append(NewLine)
+
+      builder.append(fullPath(incompat.newPath, ProtoKind.New, result.newBasePaths, result.newTree))
+      builder.append(NewLine)
+
+      builder.append(NewLine)
+    }
+
+    print(builder.result())
+    Console.flush()
+  }
+}
+
+object ConsoleProtoReport {
+
+  private val NewLine = "\n"
+
+  private sealed abstract class ProtoKind(val name: String) extends Product with Serializable {
+    def capitalizeName: String = name.capitalize
+  }
+
+  private object ProtoKind {
+    case object New extends ProtoKind("new")
+    case object Old extends ProtoKind("old")
   }
 }
